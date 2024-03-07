117968349c1e37689930c3a2642e8da5347a15d3
shimeoki <shimeoki@gmail.com>
2024-03-07 14:53:11 +0300

    fix(matrix): multiply fmt

--- internal/matrix/matrix.go
+++ internal/matrix/matrix.go
@@ -271,15 +271,15 @@ func (p *Matrix) Multiply(matrix *Matrix) (newMatrix [][]float64) {
 	rows, cols := p.Rows, matrix.Cols
 	newMatrix, memory, _ := Malloc[float64](rows, cols)
 	for i := range rows {
-		newMatrix[i] = memory[i*cols: (i+1)*cols]
+		newMatrix[i] = memory[i*cols : (i+1)*cols]
 		for j := range cols {
 			cellValue := 0.0
 			for k := range p.Cols {
-				cellValue += p.Data[i][k]*matrix.Data[k][j]
+				cellValue += p.Data[i][k] * matrix.Data[k][j]
 			}
 			newMatrix[i][j] = cellValue
 		}
 	}
 
-	return newMatrix;
+	return newMatrix
 }
